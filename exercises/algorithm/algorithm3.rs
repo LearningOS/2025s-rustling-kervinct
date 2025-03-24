4ea19c0504167cce9e24cc743c5c93e8574c7f1e
zvnlanx <kechaotai@hotmail.com>
2025-03-24 21:07:56 +0800

    finish all exercises except algorithms10 left add_node method TODO

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,13 +1,26 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T: std::cmp::PartialOrd>(array: &mut [T]) {
+    if array.len() <= 1 {
+        return;
+    }
+    for i in 0..array.len() {
+        let mut min_index = i;
+        let mut j = i + 1;
+        while j < array.len() {
+            if array[j] < array[min_index] {
+                min_index = j;
+            }
+            j += 1;
+        }
+        array.swap(i, min_index);
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,13 +31,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
